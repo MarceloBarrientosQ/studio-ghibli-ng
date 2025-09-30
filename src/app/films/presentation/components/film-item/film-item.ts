33c5f351cf19bbe6e32ef9ea63de2a51b12d62da
MarceloBarrientosQ <u20221e646@upc.edu.pe>
2025-09-30 16:18:10 -0500

    feat(all):  add new functionalities

--- src/app/films/presentation/components/film-item/film-item.ts
+++ src/app/films/presentation/components/film-item/film-item.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, inject, input} from '@angular/core';
 import {
   MatCard,
   MatCardActions,
@@ -10,6 +10,7 @@ import {
 import {MatButton} from '@angular/material/button';
 import {Film} from '../../../domain/model/film.entity';
 import {TranslatePipe} from '@ngx-translate/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-film-item',
@@ -22,4 +23,36 @@ import {TranslatePipe} from '@ngx-translate/core';
 })
 export class FilmItem {
   film = input.required<Film>();
+
+  private snackBar = inject(MatSnackBar);
+
+  async shareFilmUrl(){
+    const urlToShare= this.film().url;
+
+    if(navigator.share){
+      try {
+        await navigator.share({
+          title: this.film().title,
+          text: `Check out this film: ${this.film().title}`,
+          url: urlToShare
+        });
+        this.snackBar.open('URL shared successfully!', 'Close', { duration: 3000 });
+      }catch(e){
+        this.snackBar.open('URL shared failed!', 'Close', { duration: 3000 });
+      }
+    }else {
+      try {
+        await navigator.clipboard.writeText(urlToShare);
+        this.snackBar.open('URL copied to clipboard!', 'Close', { duration: 3000 });
+      }catch(e){
+        this.snackBar.open('Failed to copy URL!', 'Close', { duration: 3000 });
+      }
+    }
+  }
+
+  openImageInNewTab(){
+    const imageUrl = this.film().image;
+    window.open(imageUrl, '_blank');
+  }
+
 }
